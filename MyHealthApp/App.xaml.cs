3ad874209353e94d36cf35d3ca1cf19917f3637d
CarlosIzarra09 <eduizarra2@hotmail.com>
2022-09-11 15:34:15 -0500

    feat: Add Welcome and Register base page

--- MyHealthApp/App.xaml.cs
+++ MyHealthApp/App.xaml.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using MyHealthApp.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+[assembly: ExportFont("Archivo-Bold.ttf", Alias = "Archivo-Bold")]
+[assembly: ExportFont("Archivo-SemiBold.ttf", Alias = "Archivo-SemiBold")]
+[assembly: ExportFont("FjallaOne-Regular.ttf", Alias = "FjallaOne")]
+[assembly: ExportFont("Archivo-Regular.ttf", Alias = "Archivo-Regular")]
 namespace MyHealthApp
 {
     public partial class App : Application
@@ -10,7 +15,7 @@ namespace MyHealthApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage( new WelcomePage());
         }
 
         protected override void OnStart()
