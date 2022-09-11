c48d10d9b82b2aa500b105ddba6b9a05ccf21a81
CarlosIzarra09 <eduizarra2@hotmail.com>
2022-09-11 18:43:54 -0500

    feat: Add WelcomePage design

--- MyHealthApp/Views/WelcomePage.xaml.cs
+++ MyHealthApp/Views/WelcomePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace MyHealthApp.Views
@@ -9,6 +10,11 @@ namespace MyHealthApp.Views
         public WelcomePage()
         {
             InitializeComponent();
+        }
+
+        private async void StartButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new RegisterPage());
         }
     }
 }
