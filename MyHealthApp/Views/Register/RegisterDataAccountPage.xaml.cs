c41fa95bc31c14b42b137d9d7d904bfe29e4e6a7
CarlosIzarra09 <eduizarra2@hotmail.com>
2022-09-11 22:21:20 -0500

    feat: Add RegisterDataAccountPage from the flow of Register

---
+++ MyHealthApp/Views/Register/RegisterDataAccountPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyHealthApp.Views.Register
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterDataAccountPage : ContentPage
+    {
+        public RegisterDataAccountPage()
+        {
+            InitializeComponent();
+        }
+
+        private void NextButton_OnClicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
