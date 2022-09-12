f351d4f42cf99605c5059cd010b6b6ed85359e88
CarlosIzarra09 <eduizarra2@hotmail.com>
2022-09-12 04:16:14 -0500

    feat: Add Register-Credential Page from Register flow

---
+++ MyHealthApp/Views/Register/RegisterCredentialPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyHealthApp.Views.Register
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterCredentialPage : ContentPage
+    {
+        public RegisterCredentialPage()
+        {
+            InitializeComponent();
+        }
+
+        private void NextButton_OnClicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void SecondPasswordButton_OnClicked(object sender, EventArgs e)
+        {
+            this.SecondPasswordEntry.IsPassword = !this.SecondPasswordEntry.IsPassword;
+            this.SecondPasswordButton.Text = this.SecondPasswordEntry.IsPassword ? ((char)0xf070).ToString() : ((char)0xf06e).ToString();
+        }
+
+        private void FirstPasswordButton_OnClicked(object sender, EventArgs e)
+        {
+            this.FirstPasswordEntry.IsPassword = !this.FirstPasswordEntry.IsPassword;
+            this.FirstPasswordButton.Text = this.FirstPasswordEntry.IsPassword ? ((char)0xf070).ToString() : ((char)0xf06e).ToString();
+
+        }
+    }
+}
