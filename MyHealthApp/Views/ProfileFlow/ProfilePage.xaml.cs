b933467271b3f109e3eb731dccd89e6ca274fd5f
CarlosIzarra09 <eduizarra2@hotmail.com>
2022-09-14 21:09:28 -0500

    feat: Add homePage and profilePage xamarin files

---
+++ MyHealthApp/Views/ProfileFlow/ProfilePage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyHealthApp.Views.ProfileFlow
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProfilePage : ContentPage
+    {
+        public ProfilePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
