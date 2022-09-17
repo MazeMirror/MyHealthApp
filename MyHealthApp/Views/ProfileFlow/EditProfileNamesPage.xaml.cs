dd0c4b67a4b32a770fc3ce8fe5168e1ffabae12a
CarlosIzarra09 <eduizarra2@hotmail.com>
2022-09-17 14:33:59 -0500

    feat: Add view-model for EditProfileNamesPage

---
+++ MyHealthApp/Views/ProfileFlow/EditProfileNamesPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyHealthApp.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyHealthApp.Views.ProfileFlow
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditProfileNamesPage : ContentPage
+    {
+        public EditProfileNamesPage()
+        {
+            //Esto sí funcionaaaaaa ProfilePage.Model.AddElementToCollection(new ItemProfileModel(){TitleText = "Holaaa"});
+            InitializeComponent();
+        }
+    }
+}
