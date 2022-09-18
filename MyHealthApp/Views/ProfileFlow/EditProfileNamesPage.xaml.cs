b8924a00d569680cb51380b602f00b8b0421d542
CarlosIzarra09 <eduizarra2@hotmail.com>
2022-09-18 01:11:57 -0500

    feat: Add EditProfileNamesPage functional

--- MyHealthApp/Views/ProfileFlow/EditProfileNamesPage.xaml.cs
+++ MyHealthApp/Views/ProfileFlow/EditProfileNamesPage.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MyHealthApp.Models;
+using MyHealthApp.Models.SqlLite;
+using MyHealthApp.Services;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,6 +18,45 @@ namespace MyHealthApp.Views.ProfileFlow
         {
             //Esto sí funcionaaaaaa ProfilePage.Model.AddElementToCollection(new ItemProfileModel(){TitleText = "Holaaa"});
             InitializeComponent();
+            
+            EntryName.Text = ProfilePage.LocalProfile.Name;
+            EntryLastName.Text = ProfilePage.LocalProfile.LastName;
+            
+            //Los mismo que arriba pero como Xamarin Senior :V
+            //EntryName.BindingContext = ProfilePage.LocalProfile;
+            //EntryLastName.BindingContext = ProfilePage.LocalProfile;
+            //EntryName.SetBinding(Entry.TextProperty,new Binding(){Path = "Name" ,Mode = BindingMode.OneWay});
+            //EntryLastName.SetBinding(Entry.TextProperty,new Binding(){Path = "LastName",Mode = BindingMode.OneWay });
+        }
+
+        private async void LabelCancel_OnTapped(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+
+        private async void SaveChanges_OnClicked(object sender, EventArgs e)
+        {
+            var profile = ProfilePage.LocalProfile.CreateDeepCopy();
+            
+            profile.Name =  EntryName.Text;
+            profile.LastName = EntryLastName.Text;
+
+            var updatedProfile = await ProfileService.Instance.PutProfileByProfileAndId(profile,profile.Id);
+            if (updatedProfile == null)
+            {
+                await DisplayAlert("Error", "No se pudo actualizar el perfil, vuelva a intentarlo", "Ok");
+            }
+            else
+            {
+                ProfilePage.LocalProfile.Name = EntryName.Text;
+                ProfilePage.LocalProfile.LastName = EntryLastName.Text;
+                
+                await App.SqLiteDb.DeleteAllProfileAsync();
+                await App.SqLiteDb.SaveProfileAsync(ConvertToEntity.ConvertToProfileEntity(updatedProfile));
+                
+                await Navigation.PopAsync();
+            }
+           
         }
     }
 }
