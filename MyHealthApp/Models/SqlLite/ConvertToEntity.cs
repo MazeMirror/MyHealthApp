57944e64767dc6fbcb051efa99b76cb194180aeb
CarlosIzarra09 <eduizarra2@hotmail.com>
2022-09-18 01:09:29 -0500

    feat: Add EditNamesPage with functionality

---
+++ MyHealthApp/Models/SqlLite/ConvertToEntity.cs
@@ -0,0 +1,30 @@
+﻿namespace MyHealthApp.Models.SqlLite
+{
+    public static class ConvertToEntity
+    {
+        public static ProfileEntity ConvertToProfileEntity(Profile profile)
+        {
+            return new ProfileEntity()
+            {
+                Id = profile.Id,
+                Gender = profile.Gender,
+                Name = profile.Name,
+                LastName = profile.LastName,
+                BirthDate = profile.BirthDate,
+                ImageUrl = profile.ImageUrl,
+                RoleId = profile.RoleId,
+                UserId = profile.UserId,
+            };
+        }
+        
+        public static UserEntity ConvertToUserEntity(User user)
+        {
+            return new UserEntity()
+            {
+                Id = user.Id,
+                Email = user.Email,
+            };
+        }
+        
+    }
+}
